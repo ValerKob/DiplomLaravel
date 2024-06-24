fbd9f7fbf6b45923886728f275a1dc1bbef0091a
Val <89907932+ValerKob@users.noreply.github.com>
2024-06-24 23:50:07 +0300

    fvfv

--- resources/views/pairs.blade.php
+++ resources/views/pairs.blade.php
@@ -4,8 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/highlights.css') }}">
     <link rel="stylesheet" href="{{ asset('css/noty.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="shortcut icon" href="{{ asset('images/logo2.svg') }}" type="image/x-icon">
